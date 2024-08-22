75a033d9846190a21adf8275089c58e9b95a39ee
Nikola Jovic <jovnikola99@gmail.com>
2024-08-22 22:54:07 +0200

    Develop (#4)
    * feat: Adding initial Spring Boot project
    
    * feat: Adding CICD pipeline
    
    * feat: Adding Docker configuration (#3)

---
+++ src/test/java/uns/ac/rs/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package uns.ac.rs.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
