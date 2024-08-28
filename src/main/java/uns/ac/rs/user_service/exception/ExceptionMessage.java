7e054460d55e8a76588ddf58ad0b064d8e4e0023
Nikola Jovic <jovnikola99@gmail.com>
2024-08-28 21:58:39 +0200

    feat: Adding exceptions and handler

---
+++ src/main/java/uns/ac/rs/user_service/exception/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package uns.ac.rs.user_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionMessage {
+    private String message;
+}
