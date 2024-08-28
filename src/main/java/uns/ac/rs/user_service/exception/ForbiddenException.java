f5afa88805040b89cae1441ebe047ebf5b105f11
Nikola Jovic <jovnikola99@gmail.com>
2024-08-29 00:11:47 +0200

    feat: Adding exceptions and handler (#10)

---
+++ src/main/java/uns/ac/rs/user_service/exception/ForbiddenException.java
@@ -0,0 +1,11 @@
+package uns.ac.rs.user_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
