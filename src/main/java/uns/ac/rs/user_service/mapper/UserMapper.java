2101414a5443db39284dc3b8f0a987e208050fb9
Nikola Jovic <jovnikola99@gmail.com>
2024-08-28 20:23:09 +0200

    fix: Updating user mapper

--- src/main/java/uns/ac/rs/user_service/mapper/UserMapper.java
+++ src/main/java/uns/ac/rs/user_service/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 import uns.ac.rs.user_service.dto.UserDTO;
 import uns.ac.rs.user_service.dto.request.RegistrationRequest;
-import uns.ac.rs.user_service.dto.request.UserUpdateRequest;
 import uns.ac.rs.user_service.model.User;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -15,8 +14,4 @@ public interface UserMapper {
     UserDTO toDto(User user);
 
     User fromRegistrationRequest(RegistrationRequest request);
-
-    @Mapping(ignore = true, target = "role")
-    @Mapping(ignore = true, target = "password")
-    void update(@MappingTarget User user, UserUpdateRequest request);
 }
